9fef1a0306f5004ef76fe8cbcdd0b01831b1163b
Bomou He <paakmau@outlook.com>
2021-09-01 19:11:46 +0800

    test: add unit tests for BookRepo

---
+++ src/test/java/com/example/demo/repo/BookRepoTests.java
@@ -0,0 +1,63 @@
+package com.example.demo.repo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.demo.entity.Book;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRepoTests {
+    private final List<Book> books =
+            Arrays.asList(
+                    new Book(null, "Book 1", "Author 1"),
+                    new Book(null, "Book 2", "Author 2"),
+                    new Book(null, "Book 3", "Author 3"));
+
+    @Autowired private BookRepo repo;
+
+    private Map<Long, Book> bookMap;
+
+    @BeforeAll
+    static void initAll() {}
+
+    @BeforeEach
+    void init() {
+        bookMap = repo.saveAll(books).stream().collect(Collectors.toMap(Book::getId, book -> book));
+    }
+
+    @Test
+    void get() {
+        assertEquals(bookMap.size(), repo.count());
+
+        for (Book book : bookMap.values()) {
+            assertEquals(book, repo.findById(book.getId()).get());
+        }
+    }
+
+    @Test
+    void getByTitle() {
+        for (Book book : bookMap.values()) {
+            List<Book> actualBooks = repo.findByTitle(book.getTitle());
+            assertEquals(1, actualBooks.size());
+            assertEquals(book, actualBooks.get(0));
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        repo.deleteAll();
+    }
+
+    @AfterAll
+    static void tearDownAll() {}
+}
