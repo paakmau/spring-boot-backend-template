2373c830385812257fe39d9a329f8fb8fc0003d1
Bomou He <paakmau@outlook.com>
2022-02-20 05:03:34 +0800

    test: use DataJpaTest

--- src/test/java/com/example/demo/repo/BookRepoTests.java
+++ src/test/java/com/example/demo/repo/BookRepoTests.java
@@ -13,9 +13,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@SpringBootTest
+@DataJpaTest
 class BookRepoTests {
     private final List<Book> books =
             Arrays.asList(
